421166d05c0e139c422767f680866a39d22a9a12
Muhammad Ahmed Amin <ahmedaminle406@gmail.com>
2026-03-12 23:27:20 +0500

    add: streamlit_app.py entry point for Streamlit Cloud

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+"""
+Streamlit app wrapper for Streamlit Cloud deployment.
+This file is used by Streamlit Cloud as the entry point.
+"""
+
+# Import and run the main app
+exec(open("app.py").read())
